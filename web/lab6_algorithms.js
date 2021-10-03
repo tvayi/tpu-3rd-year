2dcbe34fa9ae26850b65fd693e48b1a7558939fb
Semyon Belyaev <57496334+tvayi@users.noreply.github.com>
2021-10-03 15:16:06 +0700

    Update lab6_algorithms.js

--- web/lab6_algorithms.js
+++ web/lab6_algorithms.js
@@ -1,5 +1,5 @@
 function sortLastNames(array) {
-    if (Array.isArray(array)) {
+    if (Array.isArray(array) && canBeSplitted(array)) {
       array.sort(function(a, b) {
       return a.split(' ')[1].length - b.split(' ')[1].length 
       || a.split(' ')[1].localeCompare(b.split(' ')[1]); 
@@ -7,13 +7,29 @@ function sortLastNames(array) {
     return array
     }
     else {
-        return 'Invalid input: it is not an array'
+        return 'Invalid input: not an array or strings are incorrect'
     }
 }
 
+function canBeSplitted(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] == 'string') { 
+      if (array[i].split(' ').length == 1){
+        return false
+      }
+    }
+    else {
+      return false
+    }
+  }
+  return true
+}
+ 
 
 console.log(sortLastNames(['Ben Jonson', 'Lu Kan', 'Tyler Derden']))
 console.log(sortLastNames(['Ayanami Rey', 'Sindzi Ikary', 'Ayanami Ray',]))
 console.log(sortLastNames(['Forrest Gump', 'Forrest Gump', 'Forrest Gump',]))
 console.log(sortLastNames([]))
 console.log(sortLastNames('last name'))
+console.log(sortLastNames(['BenJonson', 'Lu Kan', 'Tyler Derden']))
+console.log(sortLastNames(['BenJonson', 3, 'Tyler Derden']))
